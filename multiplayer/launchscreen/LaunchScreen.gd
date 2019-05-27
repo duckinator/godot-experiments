6374c3b4c03136e1fdab1cff7c6c84210a5a54ae
Ellen Marie Dash <me@duckie.co>
2019-05-27 17:06:56 -0400

    Add Quit button.

--- multiplayer/launchscreen/LaunchScreen.gd
+++ multiplayer/launchscreen/LaunchScreen.gd
@@ -5,10 +5,12 @@ const LOBBY = preload("res://lobby/Lobby.tscn")
 onready var vbox = $Panel/HBoxContainer/HBoxContainer/VBoxContainer
 onready var server_button = vbox.get_node("StartServer")
 onready var client_button = vbox.get_node("StartClient")
+onready var quit_button = vbox.get_node("Quit")
 
 func _ready():
 	server_button.connect("pressed", self, "start", [true])
 	client_button.connect("pressed", self, "start", [false])
+	quit_button.connect("pressed", get_tree(), "quit")
 
 func start(is_server):
 	get_tree().set_meta("starting_server", is_server)
